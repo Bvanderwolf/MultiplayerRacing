9228f526bd171eed920c79785eb4db96ed7d2a13
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-03-03 23:21:17 +0100

    Updated car prefab and added Racer script

---
+++ MultiplayerRacer/Assets/Scripts/MultiplayerRacer/Racer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace MultiplayerRacer
+{
+    public class Racer : MonoBehaviour, IPunInstantiateMagicCallback
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        public void OnPhotonInstantiate(PhotonMessageInfo info)
+        {
+            info.Sender.TagObject = this.gameObject;
+            //kan Photonview reference hieruit halen
+            //kan behouden voor references naar vanalles en nog wat
+        }
+    }
+}
--- MultiplayerRacer/Assets/Scripts/MultiplayerRacer/RoadManager.cs
+++ MultiplayerRacer/Assets/Scripts/MultiplayerRacer/RoadManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 namespace MultiplayerRacer
 {
@@ -7,23 +8,35 @@ namespace MultiplayerRacer
         [SerializeField] private GameObject[] roads;
 
         private GameObject roadOn;
+        private GameObject myCar;
 
         private void Awake()
         {
             //the player starts on the middle road
             roadOn = roads[1];
-            //place the car spawns on the road for players
-            SetupCarSpawns();
+
+            if (PhotonNetwork.IsConnected)
+            {
+                //place the car spawns on the road for players and get our own car spawn
+                Vector3 myCarSpawnPosition = SetupCarSpawns();
+                myCar = PhotonNetwork.Instantiate("prefabs/car", myCarSpawnPosition, Quaternion.identity);
+            }
+            else Debug.LogError("Wont do car setup :: not connected to photon network");
         }
 
-        private void SetupCarSpawns()
+        /// <summary>
+        /// Sets up car spawns and returns the world position of the one
+        /// used by your car. Will return zero when failed.
+        /// </summary>
+        /// <returns></returns>
+        private Vector3 SetupCarSpawns()
         {
             Transform carSpawnTransform = roadOn.transform.Find("CarSpawns");
             Transform road = roadOn.transform.Find("Road");
             if (carSpawnTransform == null || road == null)
             {
                 Debug.LogError("Wont setup car spawns :: car spawn or road is null");
-                return;
+                return Vector3.zero;
             }
             int count = carSpawnTransform.childCount;
             float width = road.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
@@ -39,6 +52,7 @@ namespace MultiplayerRacer
                 tf.localPosition = new Vector2(position.x + x, position.y);
                 tf.gameObject.SetActive(true);
             }
+            return carSpawnTransform.GetChild(InRoomManager.Instance.NumberInRoom).position;
         }
     }
 }
